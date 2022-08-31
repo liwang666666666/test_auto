e475051880d76cec22de6fc16e9f56e1c2ea0b31
liwang <109132169+liwang666666666@users.noreply.github.com>
2022-08-31 14:22:55 +0800

    Update ble.py

--- ble.py
+++ ble.py
@@ -1,11 +1,12 @@
 import sys 
 import os
-sys.path.append('/usr/bin/python3')
+# sys.path.append('/usr/bin/python3')
 import asyncio
 import async_timeout
 import binascii
 import sys
 sys.path.append(os.getcwd() + '/proto_py')
+sys.path.append("/Users/zerozero/.jenkins/workspace/ota-test")
 from proto_py import service_pb2
 
 from bleak import discover
